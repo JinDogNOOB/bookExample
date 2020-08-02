69155d767b0fb3ce925f4683704fd2cbe89eca9e
JinDogNOOB <mrriey00@naver.com>
2020-08-02 14:35:55 +0900

    fix-something

--- nodeServer/config/CowTraceAbility.js
+++ nodeServer/config/CowTraceAbility.js
@@ -1,283 +1,264 @@
 const Web3 = require('web3');
-const url = 'http://172.27.0.168:8545';
+const url = 'http://127.0.0.1:8545';
 const web3 = new Web3(url);
-const contractAddress="0x81B3b924B51f45700eC108bD71Dd5134Ae15d5ac";
+const contractAddress="0x1b6d34A796bDFE819A0B6Ee0148402EE21F19999";
 
 const contractAbi = [
 	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_momPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_dadPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_ownerPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_desc",
+		  "type": "string"
+		}
+	  ],
+	  "name": "addCow",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_momPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_dadPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_ownerPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_desc",
-				"type": "string"
-			}
-		],
-		"name": "addCow",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_workerPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_cowPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "uint32",
+		  "name": "_weight",
+		  "type": "uint32"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_desc",
+		  "type": "string"
+		}
+	  ],
+	  "name": "butcherCow",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_workerPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_cowPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "uint32",
-				"name": "_weight",
-				"type": "uint32"
-			},
-			{
-				"internalType": "string",
-				"name": "_desc",
-				"type": "string"
-			}
-		],
-		"name": "butcherCow",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_desc",
+		  "type": "string"
+		}
+	  ],
+	  "name": "changeCowDesc",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_desc",
-				"type": "string"
-			}
-		],
-		"name": "changeCowDesc",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_desc",
+		  "type": "string"
+		}
+	  ],
+	  "name": "changeMeatDesc",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_desc",
-				"type": "string"
-			}
-		],
-		"name": "changeMeatDesc",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		}
+	  ],
+	  "name": "cowDied",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			}
-		],
-		"name": "cowDied",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_workerPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_parentMeatPtr",
+		  "type": "string"
+		},
+		{
+		  "internalType": "uint32",
+		  "name": "_weight",
+		  "type": "uint32"
+		},
+		{
+		  "internalType": "string",
+		  "name": "_desc",
+		  "type": "string"
+		}
+	  ],
+	  "name": "divideMeat",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_workerPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_parentMeatPtr",
-				"type": "string"
-			},
-			{
-				"internalType": "uint32",
-				"name": "_weight",
-				"type": "uint32"
-			},
-			{
-				"internalType": "string",
-				"name": "_desc",
-				"type": "string"
-			}
-		],
-		"name": "divideMeat",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		}
+	  ],
+	  "name": "meatSelled",
+	  "outputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "function"
 	},
 	{
-		"constant": true,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			}
-		],
-		"name": "getCow",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
+	  "inputs": [],
+	  "stateMutability": "nonpayable",
+	  "type": "constructor"
 	},
 	{
-		"constant": true,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			}
-		],
-		"name": "getMeat",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "uint32",
-				"name": "",
-				"type": "uint32"
-			},
-			{
-				"internalType": "uint32",
-				"name": "",
-				"type": "uint32"
-			},
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		}
+	  ],
+	  "name": "getCow",
+	  "outputs": [
+		{
+		  "internalType": "uint8",
+		  "name": "",
+		  "type": "uint8"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		}
+	  ],
+	  "stateMutability": "view",
+	  "type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ptr",
-				"type": "string"
-			}
-		],
-		"name": "meatSelled",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+	  "inputs": [
+		{
+		  "internalType": "string",
+		  "name": "_ptr",
+		  "type": "string"
+		}
+	  ],
+	  "name": "getMeat",
+	  "outputs": [
+		{
+		  "internalType": "uint8",
+		  "name": "",
+		  "type": "uint8"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		},
+		{
+		  "internalType": "uint32",
+		  "name": "",
+		  "type": "uint32"
+		},
+		{
+		  "internalType": "uint32",
+		  "name": "",
+		  "type": "uint32"
+		},
+		{
+		  "internalType": "string",
+		  "name": "",
+		  "type": "string"
+		}
+	  ],
+	  "stateMutability": "view",
+	  "type": "function"
 	}
-]
+  ]
 
 
 module.exports = {
